735acc3c9f17d2badcff68f8d9749a7e7cfced9b
Jack Brennan <jack.brennan@nvngsystems.com>
2019-06-14 01:46:01 +0000

    add authentication to admin pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret"
   def index
     @categories = Category.order(id: :desc).all
   end

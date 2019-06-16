6956dea090701faaa08a02aa6e4dc092b769de0e
nvngsys <jack.brennan@nvngsystems.com>
2019-06-16 10:55:40 -0600

    Changes to add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,4 +7,22 @@ class ReviewsController < ApplicationController
 
     @reviews = @product.reviews
   end
+
+   def create
+    puts 'show me params'
+    puts params
+    puts "show me current user #{current_user.id}"
+    
+    @product = Product.find(params['product_id'])
+    p = @product.reviews.create!(allowed_params)
+
+    redirect_to product_path(@product)
+    end
+
+    private
+    def allowed_params
+      params.require(:review).permit(:user_id, :description, :rating)
+    end
+   
+
 end
